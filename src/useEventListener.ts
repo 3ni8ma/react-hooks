ed8b005bb92fb6aafa8b3c978dc79a797439083d
Aarush Karak <worldshaans@gmail.com>
2026-06-22 11:23:21 -0400

    Update react-hooks: new hooks 2026-06-22 11:23 [skip ci]

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -1,3 +1,4 @@
+// Update: 2026-06-22 11:23:21
 import { useEffect, useRef } from 'react';
 
 type EventMap = WindowEventMap & HTMLElementEventMap & DocumentEventMap;
--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -1,3 +1,4 @@
+// Update: 2026-06-22 11:23:21
 import { useRef, useEffect } from 'react';
 
 export function usePrevious<T>(value: T): T | undefined {
