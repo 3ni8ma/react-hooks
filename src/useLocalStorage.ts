ab2b6996fc108cc7921266c14f9c2973c82ef703
Aarush Karak <worldshaans@gmail.com>
2026-07-05 18:14:31 -0400

    Update react-hooks: 2026-07-05 18:30 [skip ci]

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,4 +1,4 @@
-// Update: 2026-07-05 18:00:12
+// Update: 2026-07-05 18:30:04
 import { useState, useCallback, useEffect } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((prev: T) => T)) => void] {
