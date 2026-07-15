e0e11c9b2c1b84b2c560c1bb403d0361db688489
Aarush Karak <worldshaans@gmail.com>
2026-07-15 18:51:05 -0400

    Update react-hooks: 2026-07-15 19:01 [skip ci]

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,4 +1,4 @@
-// Update: 2026-07-15 17:30:38
+// Update: 2026-07-15 19:01:36
 import { useState, useEffect, useRef, useCallback } from 'react';
 
 export function useDebounce<T>(value: T, delay: number = 500): [T, () => void] {
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,4 +1,4 @@
-// Update: 2026-07-15 17:30:38
+// Update: 2026-07-15 19:01:36
 import { useState, useCallback, useEffect } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((prev: T) => T)) => void] {
