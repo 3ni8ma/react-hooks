33a382b79586a3c474f3cb4be1efc72607d17f20
Aarush Karak <worldshaans@gmail.com>
2026-08-18 19:35:02 -0400

    Update react-hooks: new hooks 2026-08-18 20:30 [skip ci]

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -1,4 +1,4 @@
-// Update: 2026-08-18 19:30:18
+// Update: 2026-08-18 20:30:08
 import { useEffect, useRef } from 'react';
 
 type EventMap = WindowEventMap & HTMLElementEventMap & DocumentEventMap;
