6421867b2e15a516550c888fa3bfa91c3294450a
Aarush Karak <worldshaans@gmail.com>
2026-08-24 08:36:43 -0400

    Update react-hooks tests: 2026-08-24 09:00 [skip ci]

--- src/__tests__/useOnlineStatus.test.tsx
+++ src/__tests__/useOnlineStatus.test.tsx
@@ -1,4 +1,4 @@
-// Updated: 2026-08-24 08:30:21
+// Updated: 2026-08-24 09:00:08
 import { renderHook, act } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import { useOnlineStatus } from '../useOnlineStatus';
