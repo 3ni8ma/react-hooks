96532fe687805dd111ddc172a2f77e2bea317ccc
Aarush Karak <worldshaans@gmail.com>
2026-09-20 22:55:27 -0400

    Update react-hooks: 2026-09-20 23:00 [skip ci]

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,4 +1,4 @@
-// Update: 2026-09-20 22:30:06
+// Update: 2026-09-20 23:00:16
 import { useState, useEffect, useRef, useCallback } from 'react';
 
 export function useDebounce<T>(value: T, delay: number = 500): [T, () => void] {
